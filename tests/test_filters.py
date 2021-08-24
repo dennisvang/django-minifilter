cd12c6d4623ed6667f60b54ca2652fa06463a161
djvangerwen <dennisvangerwen@medishielddelft.com>
2021-08-24 15:57:40 +0200

    create explicit test data, refactor tests for search_filter, add tests for parameter_filter

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,22 +1,27 @@
 import string
-from datetime import timedelta
 from django.utils import timezone
 from django.test import TestCase, RequestFactory
 from tests.models import TestModel
 from minifilter.filters import search_filter, parameter_filter
 
 
+TEST_DATA = [
+    dict(name='abc', date=timezone.datetime(2021, 1, 1).date()),
+    dict(name='bcd', date=timezone.datetime(2022, 1, 1).date()),
+    dict(name='cde', date=timezone.datetime(2022, 2, 1).date()),
+    dict(name='def', date=timezone.datetime(2023, 1, 1).date()),
+    dict(name='efg', date=timezone.datetime(2023, 2, 1).date()),
+    dict(name='fgh', date=timezone.datetime(2023, 3, 1).date()),
+]
+
+
 class QueryTests(TestCase):
     def setUp(self) -> None:
         self.request_factory = RequestFactory()
-        alphabet = string.ascii_lowercase
-        name_length = 3
-        for i in range(len(alphabet) - name_length + 1):
-            name = alphabet[i:i+name_length]
-            date = timezone.now() + timedelta(hours=i*12)
-            TestModel.objects.create(name=name, date=date)
+        for data in TEST_DATA:
+            TestModel.objects.create(**data)
 
-    def test_search_filter(self):
+    def test_search_filter_name(self):
         cases = [('', TestModel.objects.count()),
                  ('abcd', 0), ('abc', 1), ('bc', 2), ('c', 3)]
         for search_text, expected_count in cases:
@@ -24,8 +29,29 @@ class QueryTests(TestCase):
                 queryset=TestModel.objects.all(),
                 request=self.request_factory.get(f'?search={search_text}'),
                 search_fields=['name'])
-            with self.subTest(search_text=search_text):
+            with self.subTest(case=search_text):
                 self.assertEqual(expected_count, queryset.count())
 
-    def test_parameter_filter(self):
-        pass
+    def test_parameter_filter_year(self):
+        cases = ['2021', '2022', '2023']
+        for year in cases:
+            expected_count = int(year[-1])
+            queryset, parameter_choices = parameter_filter(
+                queryset=TestModel.objects.all(),
+                request=self.request_factory.get(f'?year={year}'),
+                filter_parameters=[('year', 'date__year')])
+            with self.subTest(case=expected_count):
+                self.assertEqual(expected_count, queryset.count())
+                self.assertEqual(dict(year=cases), parameter_choices)
+
+    def test_parameter_filter_year_month(self):
+        queryset, parameter_choices = parameter_filter(
+            queryset=TestModel.objects.all(),
+            request=self.request_factory.get('?year=2022&month=1'),
+            filter_parameters=[
+                ('year', 'date__year'), ('month', 'date__month')])
+        self.assertEqual(1, queryset.count())
+        self.assertEqual(
+            dict(year=['2021', '2022', '2023'],
+                 month=['1', '2']),  # year 2022 only has months 1 and 2
+            parameter_choices)
